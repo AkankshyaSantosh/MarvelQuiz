a6198dd467d4f4162f40c8592ca004036257ac69
AkankshyaSantosh <67215928+AkankshyaSantosh@users.noreply.github.com>
2021-01-27 19:45:16 +0530

    Add files via upload

---
+++ js/result.js
@@ -0,0 +1,18 @@
+var points=sessionStorage.getItem("points")
+
+var head=document.getElementById("result-header")
+
+var body=document.getElementById("result-body")
+
+if (points>=5){
+    head.innerText="Good Job you hav got "+points+" out of 6"
+    body.innerText="You worked hard and it paid off! Nice work figuring out these tough questions, you earned it!"
+}
+else{
+    head.innerText="Oops! You’ve got only "+points+" out of 7 right"
+    body.innerText="Marvel have created more Avengers than even the geekiest of geeks could remember, but that doesn’t mean you shouldn’t try."
+}
+
+function reset(){
+    location.href="./index.html"
+}
